6c567c97f7614d48ee9bf0fc51b763038499cf40
ch8154 <ch8154@gmail.com>
2016-08-15 16:03:43 +0800

    Fix Model proxy.

--- grandroid-core/src/main/java/com/grasea/grandroid/adapter/OnClickable.java
+++ grandroid-core/src/main/java/com/grasea/grandroid/adapter/OnClickable.java
@@ -1,8 +1,10 @@
 package com.grasea.grandroid.adapter;
 
+import android.support.v7.widget.RecyclerView;
+
 /**
  * Created by Alan Ding on 2016/1/19.
  */
-public interface OnClickable<T extends Object> {
-    public void onItemClick(int index, T item);
+public interface OnClickable<T extends Object, VH> {
+    public void onItemClick(VH holder, int index, T item);
 }
